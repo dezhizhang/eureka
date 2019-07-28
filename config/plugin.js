7ad004da87c5d9537c069ca1e901879ff4ec93a1
dezhizhang <1541609448@qq.com>
2019-07-28 22:50:57 +0800

    修改报错

--- config/plugin.js
+++ config/plugin.js
@@ -3,12 +3,9 @@
 /** @type Egg.EggPlugin */
 module.exports = {
   // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
+  static: {
+    enable: true,
+    package: 'egg-view-ejs',
+  }
 };
 
-exports.ejs = {
-  enable: true,
-  package: 'egg-view-ejs',
-};
