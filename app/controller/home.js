461f7019a0a14ae29653ac6390b527eef91227fb
dezhizhang <1541609448@qq.com>
2019-07-29 22:06:55 +0800

    修改

--- app/controller/home.js
+++ app/controller/home.js
@@ -2,33 +2,9 @@
 
 const Controller = require('egg').Controller;
 
-
-//egg 是一个mvc的框架
-
-/*
-
-MVC：
-
-view                         视图 模板 页面的展示
-
-Controller控制器              负责处理一些业务逻辑的处理
-
-model 模型（service）         和数据打交道（查询数据库   请求数据）
-
-
-
-
-
-*/
 class HomeController extends Controller {
   async index() {
-
-
     this.ctx.body = '你好egg.js';
-
-
-    // await this.ctx.render('index')  
-
   }
 
 }
--- app/controller/news.js
+++ app/controller/news.js
@@ -5,47 +5,16 @@ const Controller = require('egg').Controller;
 class NewsController extends Controller {
   async index() {
 
-    // this.ctx.body='新闻页面';
 
-
-    //注意  await
-
-    console.log(this.config.api);
- 
     let list = await this.service.news.getNewsList();
 
     await this.ctx.render('news',{
       msg:'111',
       list
     });
-
-
-    
   }
 
-  async content() {
-    //获取get传值
-
-    // koa中如何获取get传值    ctx.query
-
-    //egg.js里面获取get传值    
-    var query=this.ctx.query;
-
-    console.log(query);
-
-    this.ctx.body='新闻详情'
-  }
-
-  async newslist(){
-
-
-    //koa获取动态路由传值   ctx.params
-    var params=this.ctx.params;
-    console.log(params);
-
-    this.ctx.body='新闻列表'
-
-  }
+  
 }
 
 module.exports = NewsController;
