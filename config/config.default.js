22873cca4ba2d4005e3e04b5223043cb9649e711
dezhizhang <1541609448@qq.com>
2019-09-07 00:02:08 +0800

    修改配置文件

--- config/config.default.js
+++ config/config.default.js
@@ -24,7 +24,7 @@ module.exports = appInfo => {
     httpOnly:true,
     encrypt:true
   }
-  
+
   //配置模板引擎
   config.view = {
     mapping: {
@@ -42,8 +42,8 @@ module.exports = appInfo => {
   config.cluster = {
     listen: {
       path: '',
-      port: 7001,
-      hostname: 'localhost',
+      port: 443,
+      hostname: '0.0.0.0',
     }
 };
 
