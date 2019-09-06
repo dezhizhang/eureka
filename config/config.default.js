963da28a153e1a67238edd30dffe12de19b53f16
dezhizhang <1541609448@qq.com>
2019-09-06 21:51:33 +0800

    新建视图

--- config/config.default.js
+++ config/config.default.js
@@ -44,12 +44,10 @@ module.exports = appInfo => {
   config.cluster = {
     listen: {
       path: '',
-      port: 443,
-      hostname: '0.0.0.0',
+      port: 7001,
+      hostname: 'localhost',
     }
 };
-
-  config.api = 'https://news.163.com';
 
   return config;
 };
