7bfb0db2a5251ea35b5dca914f486708d719910e
dezhizhang <1541609448@qq.com>
2019-09-07 22:21:56 +0800

    权限判断

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,9 +1,17 @@
 'use strict';
-
+const url = require('url');
 module.exports = (opt,app) => {
     return async function auth(ctx,next) {
         ctx.state.csrf = ctx.csrf;
-        await next();
-
+        const pathname =url.parse(ctx.request.url).pathname ;
+        if(ctx.session.userInfo) {
+            await next();
+        } else {
+            if(pathname == '/admin/login' || pathname == '/admin/doLogin' || pathname == '/admin/verify') {
+                await next();
+            } else {
+                ctx.redirect('/admin/login')
+            }
+        }
     }
 }
