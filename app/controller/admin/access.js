7904885fb3c0b5a99ea948527657bb88baab42f4
dezhizhang <1541609448@qq.com>
2019-09-12 22:18:34 +0800

    增加权限

--- app/controller/admin/access.js
+++ app/controller/admin/access.js
@@ -8,18 +8,22 @@ class AccessController extends BaseController {
     }
     //权限
     async add() {
-        await this.ctx.render('/admin/access/add');
+        //获取模块列表
+        let result = await this.ctx.model.Access.find({'module_id':'0'});
+        await this.ctx.render('/admin/access/add',{
+            list:result
+        });
     }
     //增加权限
     async doAdd() {
         let data = this.ctx.request.body;
-        console.log(data);
-        
+        let module_id = data.module_id;
+        if(module_id!='0') {//菜单或操作
+            data.module_id = this.app.mongoose.Types.ObjectId(module_id); //转换成objectId
+        } 
         let access = new  this.ctx.model.Access(data);
         await access.save();
         await this.success('/admin/access','增加权限成功');
-
-
     }
 }
 
