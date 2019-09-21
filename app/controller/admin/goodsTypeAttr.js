906bc788957de177e4052ad8d6f27cc7b0843b94
dezhizhang <1541609448@qq.com>
2019-09-21 15:12:18 +0800

    商品类型

--- app/controller/admin/goodsTypeAttr.js
+++ app/controller/admin/goodsTypeAttr.js
@@ -3,8 +3,23 @@
 const BaseController = require('./base');
 class MainController extends BaseController {
     async index() {
-        let cate_id = this.ctx.query.id;
-        let result = await this.ctx.model.GoodsTypeAttr.find({'cate_id':cate_id});
+        let cate_id =this.app.mongoose.Types.ObjectId(this.ctx.query.id);
+        // let result = await this.ctx.model.GoodsTypeAttr.find({'cate_id':cate_id});
+        let result = await this.ctx.model.GoodsTypeAttr.aggregate([
+            {
+                $lookup:{
+                    from:'goods_type',
+                    localField:'cate_id',
+                    foreignField:'_id',
+                    as:'items'
+                }
+            },
+            {
+                $match:{
+                    'cate_id':cate_id
+                }
+            }
+        ])
         await this.ctx.render('/admin/goodsTypeAttr/index',{
             list:result
         })
