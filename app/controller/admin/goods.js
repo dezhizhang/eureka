81ad751552d9bdf60f1ca0c0b05b952a3ecb93d9
dezhizhang <1541609448@qq.com>
2019-09-22 22:19:18 +0800

    修改

--- app/controller/admin/goods.js
+++ app/controller/admin/goods.js
@@ -100,7 +100,24 @@ class MainController extends BaseController {
         }
     }
     async doAdd() {
-
+        let parts = this.ctx.multipart({ autoFields: true });
+        let files = {};               
+        let stream;
+        while ((stream = await parts()) != null) {
+            if (!stream.filename) {          
+                break;
+            }       
+            let fieldname = stream.fieldname;  //file表单的名字
+            //上传图片的目录
+            let dir=await this.service.tools.getUploadFile(stream.filename);
+            let target = dir.uploadDir;
+            let writeStream = fs.createWriteStream(target);
+            await pump(stream, writeStream);  
+            files=Object.assign(files,{
+                [fieldname]:dir.saveDir    
+            })
+        }
+        console.log()
     }
     async edit() {
 
