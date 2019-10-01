5f623680380587737600085fd8d3feeff489dd7c
dezhizhang <1541609448@qq.com>
2019-10-01 17:47:58 +0800

    产品完成

--- app/controller/admin/product.js
+++ app/controller/admin/product.js
@@ -40,11 +40,38 @@ class ProductController extends BaseController {
     }
 
     async edit() {
-
+        let id = this.ctx.query.id;
+        let result = await this.ctx.model.Product.find({'_id':id});
+        await this.ctx.render('/admin/product/edit',{
+            list:result[0]
+        });
     }
 
     async doEdit() {
-
+        let parts = this.ctx.multipart({ autoFields: true });
+        let files = {};               
+        let stream;
+        while ((stream = await parts()) != null) {
+            if (!stream.filename) {          
+                break;
+            }       
+            let fieldname = stream.fieldname;  //file表单的名字
+            //上传图片的目录
+            let dir=await this.service.tools.getUploadFile(stream.filename);
+            let target = dir.uploadDir;
+            let writeStream = fs.createWriteStream(target);
+            await pump(stream, writeStream);  
+            files=Object.assign(files,{
+                [fieldname]:dir.saveDir    
+            })
+            await this.service.tools.jimpImg(target,200,200)
+        }      
+        let id=parts.field.id;
+        console.log(id);
+        
+        let updateResult=Object.assign(files,parts.field);
+        let product =await this.ctx.model.Product.updateOne({'_id':id},updateResult);
+        await this.success('/admin/product','修改产品成功');
     }
    
 }
