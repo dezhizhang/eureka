59e83bdfbbbf1f7556dac609db432b16d029fed6
dezhizhang <1541609448@qq.com>
2020-07-12 12:00:47 +0800

    提交

--- app/router.js
+++ app/router.js
@@ -171,11 +171,7 @@ module.exports = app => {
   //注册
   router.get("/regist",controller.default.regist.index);
   //注册验证验证码
-  router.get("/pass/sendCode",controller.default.login.sendCode);
-  //注册第一步
-  router.get("/register/registerStep1",controller.default.login.registerStep1);
-   //注册第二步
-  router.get("/register/registerStep2",controller.default.login.registerStep2);
+ 
   //前台验证码
   router.get("/api/default/verify",controller.default.base.verify);
   //订单页面
