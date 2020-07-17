a0a4b032d871d29c0d254bcd6ae10d511c694c7e
dezhizhang <1541609448@qq.com>
2020-07-18 01:28:20 +0800

    上传文件

--- app/service/upload.js
+++ app/service/upload.js
@@ -2,7 +2,6 @@
 const fs = require("fs");
 const path = require('path');
 const oss = require('ali-oss'); //引入阿里云
-const dayjs = require("dayjs");
 const fsToll = require('fs-extra');
 const Service = require('egg').Service;
 const sendToWormhole = require('stream-wormhole');
@@ -18,13 +17,13 @@ class UploadService extends Service {
             bucket: 'imgguicai',
             region: 'oss-cn-hangzhou',//所在地区
         });
-        await fsToll.ensureDir(path.join(this.config.uploadDir,'app/public/admin/upload/')); //生成文件夹 ，如果存在则不生成
+        await fsToll.ensureDir(path.join('app/public/admin/upload/')); //生成文件夹 ，如果存在则不生成
         const stream = await this.ctx.getFileStream();
         const extname = path.extname(stream.filename).toLowerCase();//文件扩展名称
         const fileName = Date.now() + '' + Number.parseInt(Math.random() * 10000) + extname;//文件名
-        const target = path.join(this.config.baseDir,'app/public/admin/upload/',fileName); //文件存放目录位置
+        const target = path.join('app/public/admin/upload/',fileName); //文件存放目录位置
         const writeStream = fs.createWriteStream(target); //存储文件 创造可写流
-        const streamPipe = stream.pipe(writeStream); //文件存储等待机制 将可读性流写入可写流
+        //const streamPipe = stream.pipe(writeStream); //文件存储等待机制 将可读性流写入可写流
         try{
             const result = await client.put(fileName,target); //阿里云图片上传
             let fields = {};
