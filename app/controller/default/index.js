b31c2a7e9eb4cdf6e699018a52e49da1d717bb20
dezhizhang <1541609448@qq.com>
2020-07-18 15:22:15 +0800

    修改广告图

--- app/controller/default/index.js
+++ app/controller/default/index.js
@@ -4,8 +4,10 @@ const Controller = require('egg').Controller;
 class IndexController extends Controller {
     async index() {
         let focus = await this.ctx.model.Focus.find({'type':'1'});
+        let advert = await this.ctx.model.Advert.find({'type':'1'});
         await this.ctx.render("/default/index",{
             focus,
+            advert,
         })
     }
 }
