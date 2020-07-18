48afaf13e5a484d6f67c1b7e797484bf57b474bc
dezhizhang <1541609448@qq.com>
2020-07-18 18:01:40 +0800

    修改囷片

--- app/controller/admin/focus.js
+++ app/controller/admin/focus.js
@@ -30,30 +30,20 @@ class FocusController extends BaseController {
     }
     //修改提交数据
     async doEdit() {
-        let result = await this.service.upload.deleteImg();
-        // let parts = this.ctx.multipart({ autoFields: true });
-        // let files = {};               
-        // let stream;
-        // while ((stream = await parts()) != null) {
-        //     if (!stream.filename) {          
-        //       break;
-        //     }       
-        //     let fieldname = stream.fieldname;  //file表单的名字
-        //     //上传图片的目录
-        //     let dir=await this.service.tools.getUploadFile(stream.filename);
-        //     let target = dir.uploadDir;
-        //     let writeStream = fs.createWriteStream(target);
-        //     await pump(stream, writeStream);  
-        //     files=Object.assign(files,{
-        //       [fieldname]:dir.saveDir    
-        //     })
-            
-        // }      
-        // //修改操作
-        // let id=parts.field.id;
-        // let updateResult=Object.assign(files,parts.field);
-        // let result =await this.ctx.model.Focus.updateOne({"_id":id},updateResult);
-        // await this.success('/admin/focus','修改轮播图成功');
+        let parts = this.ctx.multipart({ autoFields: true });
+        let fields = await parts();
+        let { id } =  parts.field;
+        if(fields.filename) {//当前有图片上传先删除再上传
+            let focus = await this.ctx.model.Focus.find({"_id":id});
+            let file_name = focus[0].file_name;
+            await this.service.upload.deleteImg(file_name); //删除线上图片
+            let result = await this.service.upload.uploadImg();
+            await this.ctx.model.Focus.updateOne({"_id":id},result);
+            await this.success('/admin/focus','修改轮播图成功');
+        } else { //没有图片上传 
+            await this.ctx.model.Focus.updateOne({"_id":id},parts.field);
+            await this.success('/admin/focus','修改轮播图成功'); 
+        }
     }
     async delete() {
         let { id } = this.ctx.query;
