f535e4df99966914cb50ed8eab1b23874d1cd76c
dezhizhang <1541609448@qq.com>
2020-07-18 20:21:41 +0800

    小程序增加分类

--- app/controller/api/classify.js
+++ app/controller/api/classify.js
@@ -3,16 +3,28 @@
 const Controller = require('egg').Controller;
 class ClassifyController extends Controller {
     async index() {
-        let result = await this.ctx.model.Classify.find();
+        let result = await this.ctx.model.GoodsCate.aggregate([
+            {
+                $lookup:{
+                    from:'goods_cate',
+                    localField:'_id',
+                    foreignField:'pid',
+                    as:'items'
+                }
+            },
+            {
+                $match:{
+                    'pid':'0'
+                }
+            }
+        ]);
         this.ctx.body = {
             code:200,
-            msg:'请求成功',
+            msg:"请求成功",
             success:true,
-            data:result
+            data:result,
         }
-       
     }
 
 }
-
 module.exports = ClassifyController;
