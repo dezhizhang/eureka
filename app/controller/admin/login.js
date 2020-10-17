7bdb8bb51799a5338d0c8ce18ffcd434c25c844e
zhangdezhi <1541609448@qq.com>
2020-10-17 17:28:38 +0800

    重构登录完成

--- app/controller/admin/login.js
+++ app/controller/admin/login.js
@@ -12,20 +12,17 @@ class LoginController extends BaseController {
         const admin = this.ctx.request.body;
         const {email,password,code} = admin;
         admin.password = await this.service.tools.md5(password);
-        if(code.toUpperCase() == this.ctx.session.code.toUpperCase()) {
-            let result = await this.ctx.model.Admin.find({'email':email,'password':password});
-            if(result.length > 0) {
-                //保存用户信息
-                this.ctx.session.userInfo = result[0];
-                //跳转到用户中心
-                this.ctx.redirect('/admin/manager'); 
-            } else {
-                await this.error('/admin/login','用户名或密码不正确')
-            }
-
-        } else {
+        if(code.toUpperCase() !== this.ctx.session.code.toUpperCase()) {
             await this.error('/admin/login','验证码不正确');
+            return;
         }
+        let result = await this.ctx.model.Admin.find({'email':email,'password':admin.password});
+        if(result.length <= 0) {
+            await this.error('/admin/login','用户名或密码不正确');
+            return;
+        }
+        this.ctx.session.userInfo = result[0];
+        this.ctx.redirect('/admin/manager');   
     }
     //退出登录
     async loginOut() {
