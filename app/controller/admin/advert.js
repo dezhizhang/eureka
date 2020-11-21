23d3addca63826d649927d389ba1254f5efec17d
zhangdezhi <1541609448@qq.com>
2020-11-21 09:28:55 +0800

    广告管理

--- app/controller/admin/advert.js
+++ app/controller/admin/advert.js
@@ -7,12 +7,12 @@ const BaseController = require('./base');
 class AdvertController extends BaseController {
     async index() {
         let result = await this.ctx.model.Advert.find();
-        await this.ctx.render('/admin/advert/index',{
+        await this.ctx.render('/back/advert/index',{
             list:result
         });
     }
     async add() {
-        await this.ctx.render('/admin/advert/add');
+        await this.ctx.render('/back/advert/add');
         
     }
     //轮播图交数据
@@ -20,41 +20,18 @@ class AdvertController extends BaseController {
         let result = await this.service.upload.uploadImg();
         let advert =new this.ctx.model.Advert(result);
         await advert.save();
-        await this.success('/admin/advert','增加广告图成功');
+        await this.success('/back/advert','增加广告图成功');
     }
     //修改
     async edit() {
         let id = this.ctx.query.id;
         let result = await this.ctx.model.Advert.find({'_id':id});
-        await this.ctx.render('/admin/advert/edit',{
+        await this.ctx.render('/back/advert/edit',{
             list:result[0]
         });
     }
     //修改提交数据
-    async doEdit() {
-        // let parts = this.ctx.multipart({ autoFields: true });
-        // let files = {};               
-        // let stream;
-        // while ((stream = await parts()) != null) {
-        //     if (!stream.filename) {          
-        //       break;
-        //     }       
-        //     let fieldname = stream.fieldname;  //file表单的名字
-        //     //上传图片的目录
-        //     let dir=await this.service.tools.getUploadFile(stream.filename);
-        //     let target = dir.uploadDir;
-        //     let writeStream = fs.createWriteStream(target);
-        //     await pump(stream, writeStream);  
-        //     files=Object.assign(files,{
-        //       [fieldname]:dir.saveDir    
-        //     })
-            
-        // }      
-        // //修改操作
-        // let id=parts.field.id;
-        // let updateResult=Object.assign(files,parts.field);
-        // let result =await this.ctx.model.Advert.updateOne({"_id":id},updateResult);
-        // await this.success('/admin/advert','修改广告成功');
+    async doEdit() { 
         let parts = this.ctx.multipart({ autoFields: true });
         let fields = await parts();
         let { id,file_name } =  parts.field;
