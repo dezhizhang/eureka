3f030fdaa83daa1783bd20b6312a990844947b03
zhangdezhi <1541609448@qq.com>
2020-11-29 11:48:19 +0800

    颜色管理

--- app/controller/admin/goodsColor.js
+++ app/controller/admin/goodsColor.js
@@ -3,13 +3,17 @@
 const BaseController = require('./base');
 class GoodsColorController extends BaseController {
     async index() {
-        let result = await this.ctx.model.GoodsColor.find();
-        await this.ctx.render('/admin/goodsColor/index',{
-            list:result
+        let page = this.ctx.query.page || 1;
+        let pageSize = 10;
+        let count = await this.ctx.model.GoodsColor.find().count();
+        let result = await this.ctx.model.GoodsColor.find().limit(pageSize).skip((page - 1) * pageSize);
+        await this.ctx.render('/back/goodsColor/index',{
+            list:result,
+            count:count,
         });
     }
     async add() {
-        await this.ctx.render('/admin/goodsColor/add');
+        await this.ctx.render('/back/goodsColor/add');
     }
     async doAdd() {
         let result = this.ctx.request.body;
@@ -20,7 +24,7 @@ class GoodsColorController extends BaseController {
     async edit() {
         let id = this.ctx.query.id
         let result = await this.ctx.model.GoodsColor.find({'_id':id});
-        await this.ctx.render('/admin/goodsColor/edit',{
+        await this.ctx.render('/back/goodsColor/edit',{
             list:result[0]
         })
     }
@@ -30,7 +34,6 @@ class GoodsColorController extends BaseController {
         let goodsColor = await this.ctx.model.GoodsColor.updateOne({'_id':id},result);
         await this.success('/admin/goodsColor','修改商品颜色成功');
     }
-  
 }
 
 module.exports = GoodsColorController
