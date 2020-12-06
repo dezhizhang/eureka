3b21f98426925b4e8f05a7ac23a39a926bb6a802
zhangdezhi <1541609448@qq.com>
2020-12-06 14:17:25 +0800

    增加表

---
+++ app/model/evaluation.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = app => {
+    const mongoose = app.mongoose;
+    const Schema = mongoose.Schema;
+    const d = new Date();
+    const EvaluationSchema = Schema({
+        openid:{ type:String },
+        userName:{ type:String },
+        mobile:{ type:String },
+        description:{ type:String },
+        add_time:{
+           type:Number,
+           default:d.getTime()
+        },
+    });
+    return mongoose.model('Evaluation',EvaluationSchema,'evaluation');
+}
