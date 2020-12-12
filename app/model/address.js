8acfa8329c8a37dd4cfb9b5d469139f43bae3783
zhangdezhi <1541609448@qq.com>
2020-12-12 17:59:23 +0800

    新增用户地址

---
+++ app/model/address.js
@@ -0,0 +1,29 @@
+/**
+ * @author:zhangdezhi
+ * @date:2020-07-12
+ * @desc:用户模型
+*/
+'use strict';
+module.exports = app => {
+    const mongoose = app.mongoose;
+    const Schema = mongoose.Schema;
+    const d = new Date();
+    const AddressSchema = new Schema({
+        url:{ type:String }, //用户头像
+        email:{ type:String }, //企业邮箱
+        mobile:{ type:String },
+        openid:{ type:String }, //用于小程序端登录
+        userName:{ type:String },
+        address:{ type:String },
+        isDefault:{ type:Number,default:0},//用户会员类型
+        status:{
+            type:Number,
+            default:1 //用户当前状态
+        },
+        add_time:{
+            type:Number,
+            default:d.getTime()
+        }
+    });
+    return mongoose.model('Address',AddressSchema,'address');
+}
